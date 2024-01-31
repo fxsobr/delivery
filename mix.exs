e96792ba4acbf1f989850d5b26ff59a18a4bebff
Marcelo Ceolin <fxsobr@gmail.com>
2024-01-31 20:00:53 -0300

    criado changeset

--- mix.exs
+++ mix.exs
@@ -44,7 +44,9 @@ defmodule Delivery.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:comeonin, "~> 5.0"},
+      {:pbkdf2_elixir, "~> 2.0"}
     ]
   end
 
